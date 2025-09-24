426a41a3b3a171e54fae72d15ed60ff7e5e6dd4c
Icaro Caetano <35819169+IcaroCaetano@users.noreply.github.com>
2025-09-24 14:18:12 -0700

    Update JfrProfiler.java

--- src/main/java/com/myprojecticaro/poc_features_java_25/features/JfrProfiler.java
+++ src/main/java/com/myprojecticaro/poc_features_java_25/features/JfrProfiler.java
@@ -1,1 +1,13 @@
 
+public class JfrProfilerExample {
+
+
+  public static void startRecording() {
+
+    System.out.println(" (stub) start JFR recording - enable via JVM flags");
+  }
+
+  public static void stopRecording() {
+    System.out.println(" (stub) stop JFR recording");
+  }
+}
