c7986e625bd3cfc67de1abd62ff04569864429a8
Icaro Caetano <35819169+IcaroCaetano@users.noreply.github.com>
2025-09-24 14:07:54 -0700

    Update FlexibleConstructor.java

--- src/main/java/com/myprojecticaro/poc_features_java_25/features/FlexibleConstructor.java
+++ src/main/java/com/myprojecticaro/poc_features_java_25/features/FlexibleConstructor.java
@@ -1,27 +1,37 @@
 package com.myprojecticaro.poc_features_java_25.features;
 
 class ParentForFlexible {
-private final int value;
-ParentForFlexible(int value) {
-this.value = value;
-}
-public int getValue() { return value; }
+
+  private final int value;
+
+  ParentForFlexible(int value) {
+
+    this.value = value;
+
+  }
+
+  public int getValue() { return value; }
+
 }
 
 
 public class FlexibleConstructorExample extends ParentForFlexible {
-private final int value;
+
+  private final int value;
 
 
 public FlexibleConstructorExample(int value) {
+  
+  if (value < 0) {
 
-if (value < 0) {
-throw new IllegalArgumentException("value must be >= 0");
-}
-this.value = value * 1; 
-super(value); 
+    throw new IllegalArgumentException("value must be >= 0");
 }
 
+  this.value = value * 1; 
+  super(value); 
+}
 
-public int getValue() { return value; }
+
+  public int getValue() { return value; }
+
 }
