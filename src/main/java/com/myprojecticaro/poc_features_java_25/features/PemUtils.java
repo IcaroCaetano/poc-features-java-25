257ea03eef00e92971eb4078fd8b4dff5eb07246
Icaro Caetano <35819169+IcaroCaetano@users.noreply.github.com>
2025-09-24 14:16:08 -0700

    Update PemUtils.java

--- src/main/java/com/myprojecticaro/poc_features_java_25/features/PemUtils.java
+++ src/main/java/com/myprojecticaro/poc_features_java_25/features/PemUtils.java
@@ -1,4 +1,4 @@
-
+package com.myprojecticaro.poc_features_java_25.features;
 
 import java.util.Base64;
 
@@ -7,21 +7,25 @@ private PemUtils() {}
 
 
 public static String wrapPem(String type, String base64Body) {
-StringBuilder sb = new StringBuilder();
-sb.append("-----BEGIN ").append(type).append("-----\n");
-// quebra simples a cada 64 chars
-int i = 0;
-while (i < base64Body.length()) {
-int end = Math.min(i + 64, base64Body.length());
-sb.append(base64Body, i, end).append('\n');
-i = end;
-}
-sb.append("-----END ").append(type).append("-----\n");
-return sb.toString();
+  
+  StringBuilder sb = new StringBuilder();
+  sb.append("-----BEGIN ").append(type).append("-----\n");
+
+  int i = 0;
+  
+  while (i < base64Body.length()) {
+    int end = Math.min(i + 64, base64Body.length());
+    sb.append(base64Body, i, end).append('\n');
+    i = end;
+  }
+  
+  sb.append("-----END ").append(type).append("-----\n");
+
+  return sb.toString();
+  
 }
 
-
-public static String encodeToBase64(String plain) {
-return Base64.getEncoder().encodeToString(plain.getBytes());
-}
+  public static String encodeToBase64(String plain) {
+    return Base64.getEncoder().encodeToString(plain.getBytes());
+  }
 }
