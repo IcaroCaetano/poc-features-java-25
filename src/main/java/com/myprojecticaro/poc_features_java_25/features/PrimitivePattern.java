9ee3ec64444fee0c990dcf66b90cac85aa152776
Icaro Caetano <35819169+IcaroCaetano@users.noreply.github.com>
2025-09-24 14:09:35 -0700

    Update PrimitivePattern.java

--- src/main/java/com/myprojecticaro/poc_features_java_25/features/PrimitivePattern.java
+++ src/main/java/com/myprojecticaro/poc_features_java_25/features/PrimitivePattern.java
@@ -1,1 +1,23 @@
 
+public class PrimitivePatternExample {
+  
+  public static void runExample() {
+  
+    Object[] inputs = { 10, 3.14, "hello", (short)7 };
+
+
+  for (Object o : inputs) {
+    String result = switch (o) { 
+    case int i -> "int: " + i;
+    case double d -> "double: " + d;
+    case short s -> "short: " + s;
+    case String s -> "string: " + s;
+    default -> "unknown: " + o;
+  };
+
+    System.out.println(" -> " + result);
+
+  }
+
+  }
+}
