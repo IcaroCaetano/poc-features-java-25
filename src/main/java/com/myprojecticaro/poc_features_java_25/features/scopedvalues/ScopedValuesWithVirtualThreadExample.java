f75585125c7a24d89c58cd49725fd3cacaad8b86
Icaro Caetano <35819169+IcaroCaetano@users.noreply.github.com>
2026-04-26 19:00:11 -0700

    Add ScopedValuesWithVirtualThreadExample class
    Implement example of using scoped values with virtual threads in Java.

--- src/main/java/com/myprojecticaro/poc_features_java_25/features/scopedvalues/ScopedValuesWithVirtualThreadExample.java
+++ src/main/java/com/myprojecticaro/poc_features_java_25/features/scopedvalues/ScopedValuesWithVirtualThreadExample.java
@@ -1,1 +1,31 @@
+package com.myprojecticaro.poc_features_java_25.features.scopedvalues;
+
+import java.lang.ScopedValue;
+import java.util.concurrent.Executors;
+
+public class ScopedValuesWithVirtualThreadExample {
+
+    private static final ScopedValue<RequestContext> CONTEXT =
+            ScopedValue.newInstance();
+
+    public void run() throws Exception {
+        System.out.println("Running scoped values with virtual threads");
+
+        try (var executor = Executors.newVirtualThreadPerTaskExecutor()) {
+
+            ScopedValue.where(
+                    CONTEXT,
+                    new RequestContext("req-vt-999", "virtual-user")
+            ).run(() -> {
+                executor.submit(this::asyncOperation).join();
+            });
+        }
+    }
+
+    private void asyncOperation() {
+        System.out.println(
+                "Virtual Thread context: " + CONTEXT.get()
+        );
+    }
+}
 
