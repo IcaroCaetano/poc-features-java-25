4846ec2d9de94a6b4396018c22db0eae6882be24
Icaro Caetano <35819169+IcaroCaetano@users.noreply.github.com>
2026-04-26 18:57:40 -0700

    Update ScopedValuesBasicExample.java

--- src/main/java/com/myprojecticaro/poc_features_java_25/features/scopedvalues/ScopedValuesBasicExample.java
+++ src/main/java/com/myprojecticaro/poc_features_java_25/features/scopedvalues/ScopedValuesBasicExample.java
@@ -1,1 +1,30 @@
+package com.myprojecticaro.poc_features_java_25.features.scopedvalues;
+
+import java.lang.ScopedValue;
+
+public class ScopedValuesBasicExample {
+
+    private static final ScopedValue<RequestContext> CONTEXT =
+            ScopedValue.newInstance();
+
+    public void run() {
+        System.out.println("Running basic scoped value example");
+
+        ScopedValue.where(
+                CONTEXT,
+                new RequestContext("req-123", "icaro")
+        ).run(this::serviceLayer);
+    }
+
+    private void serviceLayer() {
+        repositoryLayer();
+    }
+
+    private void repositoryLayer() {
+        RequestContext context = CONTEXT.get();
+        System.out.println(
+                "Context available in repository: " + context
+        );
+    }
+}
 
